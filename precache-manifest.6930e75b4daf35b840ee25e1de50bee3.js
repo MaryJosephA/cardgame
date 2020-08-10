2d1ef6f77a06dc03f15cd39ddbbc8ce97ce82d70
Mary Abraham <maryabraham@Marys-MBP.fios-router.home>
2020-08-09 20:33:47 -0400

    Updates

---
+++ precache-manifest.6930e75b4daf35b840ee25e1de50bee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ca47d1098b8cf930f5d4579a7e4c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aff2fe416abbffb9cf7",
+    "url": "/static/css/main.7b6ab773.chunk.css"
+  },
+  {
+    "revision": "8ffa74cfded36ec1a260",
+    "url": "/static/js/2.3ef28f30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ef28f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aff2fe416abbffb9cf7",
+    "url": "/static/js/main.9e48aa96.chunk.js"
+  },
+  {
+    "revision": "32b4edc6d51f459c05a4",
+    "url": "/static/js/runtime-main.e742d535.js"
+  }
+]);
